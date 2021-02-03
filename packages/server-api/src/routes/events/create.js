e526a0ef2865359c75e80c6b3ae71675f2a56f8d
Sasha Jolich <sasha@ctoatlarge.com>
2021-02-03 01:19:10 -0800

    POST/PUT to create/update events endpoint simplified
    Endpoint path is now /event instead of /db/ns/stream/event

--- packages/server-api/src/routes/events/create.js
+++ packages/server-api/src/routes/events/create.js
@@ -2,10 +2,11 @@ const { Account } = require('@persistr/server-fluent')
 const uuidv4 = require('uuid/v4')
 module.exports = {
   method: 'post',
-  path: '/db/:db/ns/:ns/streams/:stream/events',
+  path: '/events',
   schema: 'events/write',
   handler: async (req, res) => {
-    let event = await Account.from(req.credentials).db(req.params.db).ns(req.params.ns).stream(req.params.stream).events().write({ id: uuidv4(), data: req.body.data, meta: req.body.meta })
+    const event = req.body
+    await Account.from(req.credentials).db(event.meta.db).ns(event.meta.ns).stream(event.meta.stream).events().write({ id: uuidv4(), data: event.data, meta: event.meta })
     res.status(201).send()
   }
 }
--- packages/server-api/src/routes/events/update.js
+++ packages/server-api/src/routes/events/update.js
@@ -1,10 +1,11 @@
 const { Account } = require('@persistr/server-fluent')
 module.exports = {
   method: 'put',
-  path: '/db/:db/ns/:ns/streams/:stream/events/:event',
+  path: '/events/:event',
   schema: 'events/write',
   handler: async (req, res) => {
-    let event = await Account.from(req.credentials).db(req.params.db).ns(req.params.ns).stream(req.params.stream).events().write({ id: req.params.event, data: req.body.data, meta: req.body.meta })
+    const event = req.body
+    await Account.from(req.credentials).db(event.meta.db).ns(event.meta.ns).stream(event.meta.stream).events().write({ id: event.meta.id, data: event.data, meta: event.meta })
     res.status(201).send()
   }
 }
