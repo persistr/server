6a7c09c3313073400d306cabadd7007670b13c15
Sasha Jolich <sasha@ctoatlarge.com>
2021-02-12 20:07:27 -0800

    Allow empty passwords

--- packages/server-crypto/src/crypto/passwords.js
+++ packages/server-crypto/src/crypto/passwords.js
@@ -10,6 +10,7 @@ async function hash (password) {
 
 async function verify ({ password, against: accountPassword }) {
   return new Promise(function (resolve, reject) {
+    if (password === '' && accountPassword === '') { resolve(true); return }
     credentials(password).verifyAgainst(accountPassword, function (error, verified) {
       if (error) { resolve(false); return }
       if (!verified) { resolve(false); return }
