5589a43901fae9682e67ebda199273b20fb75f8b
Sasha Jolich <sasha@ctoatlarge.com>
2021-02-12 20:23:09 -0800

    Replaced Account with Connection

--- packages/server-api/src/routes/accounts/get.js
+++ packages/server-api/src/routes/accounts/get.js
@@ -1,9 +1,9 @@
-const { Account } = require('@persistr/server-fluent')
+const { Connection } = require('@persistr/server-fluent')
 module.exports = {
   method: 'get',
   path: '/accounts/current',
   handler: async (req, res) => {
-    const profile = await Account.from(req.credentials).profile()
+    const profile = await Connection.from(req.credentials).account().profile()
     res.status(200).json({ id: profile.id, username: profile.username, name: profile.name, dbs: profile.dbs })
   }
 }
