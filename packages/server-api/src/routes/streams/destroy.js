20469825ce3567c938d629dedac4781ff3e28ffa
Sasha Jolich <sasha@ctoatlarge.com>
2021-03-24 15:19:17 -0700

    Made namespace optional in the API route when deleting streams

--- packages/server-api/src/routes/streams/destroy.js
+++ packages/server-api/src/routes/streams/destroy.js
@@ -1,9 +1,9 @@
 const { Connection } = require('@persistr/server-fluent')
 module.exports = {
   method: 'delete',
-  path: '/db/:db/ns/:ns/streams/:stream',
+  path: '/db/:db/streams/:stream',
   handler: async (req, res) => {
-    await Connection.from(req.credentials).db(req.params.db).ns(req.params.ns).stream(req.params.stream).destroy()
+    await Connection.from(req.credentials).db(req.params.db).ns(req.query.ns ?? '').stream(req.params.stream).destroy()
     res.status(204).send()
   }
 }
